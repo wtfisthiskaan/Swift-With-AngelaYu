23f86a9325612ebb62905273cf2ed629edf15f7d
wtfisthiskaan <78760338+wtfisthiskaan@users.noreply.github.com>
2022-08-08 14:04:20 +0300

    i am rich app
    i am rich app written with Swift

---
+++ I am rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am rich
+//
+//  Created by Kaan Uslu on 28.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
