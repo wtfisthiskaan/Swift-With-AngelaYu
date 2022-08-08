4e6e24fcd7e6254ca40b09755080e0a20befd869
wtfisthiskaan <78760338+wtfisthiskaan@users.noreply.github.com>
2022-08-08 14:05:36 +0300

    i am poor app
    i am poor app written with Swift

---
+++ I am poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am poor
+//
+//  Created by Kaan Uslu on 30.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
