0cb5cbf1dc0301c8a002cd0e687d02a869c43777
wtfisthiskaan <78760338+wtfisthiskaan@users.noreply.github.com>
2022-08-24 14:28:42 +0300

    FlashChat App
    FlashChat App with Swift

---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Kaan Uslu on 22.08.2022.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message{
+    let sender: String
+    let body: String
+}
