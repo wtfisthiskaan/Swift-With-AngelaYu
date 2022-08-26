27960f1f5c2720ddf2aa05fd8cf1ba03453749bb
wtfisthiskaan <78760338+wtfisthiskaan@users.noreply.github.com>
2022-08-26 19:17:49 +0300

    I am rich app with SwiftUI
    I am rich basic app written with SwiftUI

---
+++ I Am Rich - SwiftUI/I_Am_RichApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_Am_RichApp.swift
+//  I Am Rich
+//
+//  Created by Kaan Uslu on 25.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct I_Am_RichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
